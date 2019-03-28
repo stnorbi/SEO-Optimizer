07e065100245824238a6ac951280142fd818f5e5
StNorbi <streling.norbert@gmail.com>
2019-03-28 10:06:18 +0100

    TableWidget signal has been emited and TableWidget has got an own class separately.

--- Modules/analysView.py
+++ Modules/analysView.py
@@ -10,47 +10,57 @@ import os
 from Modules import widgets, docView
 
 class AnalysView(QWidget):
+    wordsChange=SIGNAL('list')
+
     def __init__(self,mainwindow):
         QWidget.__init__(self)
+        self.mainWindow=mainwindow
         self.resize(500,500)
         self.setLayout(QHBoxLayout())
         self.layout().setContentsMargins(0,10,10,10)
         self.setContextMenuPolicy(Qt.CustomContextMenu)
 
-        self.mainWindow=mainwindow
-
-
         AnalysLayout=QVBoxLayout()
         self.layout().addLayout(AnalysLayout)
 
-        self.analyserTable=QTableWidget()
-        self.analyserTable.keyboardGrabber()
-        self.analyserTable.setUpdatesEnabled(True)
-        self.analyserTable.setColumnCount(7)
-        self.analyserTable.setRowCount(1)
-        self.analyserTable.resize(400,250)
-        self.analyserTable.setHorizontalHeaderLabels(("Szavak;"
-                                                      "Megjelenítés;"
-                                                      "Pozíció;"
-                                                      "CTR;"
-                                                      "Kattintás;"
-                                                      "Tartalmazza?;"
-                                                      ).split(";"))
+        self.analyserTable=TableWidget(self)
         AnalysLayout.addWidget(self.analyserTable)
 
         separator = widgets.Separator("horizontal")
 
-
-    def setWords(self):
-        allRows = self.analyserTable.rowCount()
-        if self.analyserTable.cellWidget(1, 1):
-            for row in range(0,allRows):
-                word=self.analyserTable.cellWidget(row,1)
-                print(word)
+        self.analyserTable.cellChanged.connect(self.analyserTable.setWordList)
 
 
+class TableWidget(QTableWidget):
+    def __init__(self,parent):
+        QTableWidget.__init__(self,parent)
+        self.keyboardGrabber()
+        self.setUpdatesEnabled(True)
+        self.setColumnCount(7)
+        self.setRowCount(2)
+        self.resize(400,250)
+        self.setHorizontalHeaderLabels(("Szavak;"
+                                      "Megjelenítés;"
+                                      "Pozíció;"
+                                      "CTR;"
+                                      "Kattintás;"
+                                      "Tartalmazza?;"
+                                      ).split(";"))
 
-    def addNewRow(self,rows):
-        if self.analyserTable.cellWidget(rows,1):
-            rows+=1
-            self.analyserTable.setRowCount(rows)
+
+    def setWordList(self):
+        wordList=[]
+        allRows = self.rowCount()
+        self.setRowCount(allRows+1)
+        if self.item(0, 0).text():
+            for row in range(0,allRows-1):
+                wordList.append(self.item(row,0).text())
+        return wordList
+
+
+
+    def addNewRow(self):
+            self.insertRow(1)
+
+    def refresh(self):
+        self.clear()
